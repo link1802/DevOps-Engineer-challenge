066a36d7a032fa50cdb0b3100e4e626c7025a935
link1802 <link1802@gmail.com>
2022-05-12 23:49:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
   target                = google_compute_region_target_http_proxy.default.id
   network               = google_compute_network.exam-network.id
   subnetwork            = google_compute_subnetwork.exam-subnet.id
-  network_tier          = "PREMIUM"
+  network_tier          = "STANDARD"
 }
 
 # HTTP target proxy
