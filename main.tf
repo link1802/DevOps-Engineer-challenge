1edc1c56cd3371731950055c9d2ffc145fcac4f3
link1802 <link1802@gmail.com>
2022-05-12 21:37:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,7 +96,7 @@ resource "google_compute_region_backend_service" "default" {
   timeout_sec           = 10
   health_checks         = [google_compute_region_health_check.default.id]
   backend {
-    group           = google_compute_region_instance_group_manager.mig.instance_group
+    group           = google_compute_instance_group_manager.exam
     balancing_mode  = "UTILIZATION"
     capacity_scaler = 1.0
   }
