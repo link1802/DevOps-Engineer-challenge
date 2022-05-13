0e11118c991584ae30c1be320d86d724b4b86313
link1802 <link1802@gmail.com>
2022-05-12 21:39:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,13 +51,13 @@ resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
   name                  = "exam-forwarding-rule"
   provider              = google-beta
   region                = "us-central1"
-  depends_on            = [google_compute_subnetwork.proxy_subnet]
+  depends_on            = [google_compute_subnetwork.exam-proxy-subnet]
   ip_protocol           = "TCP"
   load_balancing_scheme = "INTERNAL_MANAGED"
   port_range            = "80"
   target                = google_compute_region_target_http_proxy.default.id
-  network               = google_compute_network.ilb_network.id
-  subnetwork            = google_compute_subnetwork.ilb_subnet.id
+  network               = google_compute_network.exam-network.id
+  subnetwork            = google_compute_subnetwork.exam-subnet.id
   network_tier          = "STANDARD"
 }
 
