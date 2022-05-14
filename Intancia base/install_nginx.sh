88ad34fbeb7b1eb2f6f75eb15c05a3b0067723c9
link1802 <link1802@gmail.com>
2022-05-14 00:06:37 -0500

    Update install_nginx.sh

--- Intancia base/install_nginx.sh
+++ Intancia base/install_nginx.sh
@@ -3,6 +3,11 @@
  export DEBIAN_FRONTEND=noninteractive
  apt-get update -y
  apt-get install ufw -y
+ ufw allow 'Nginx HTTP'
+ ufw allow 'SSH'
+ ufw enable
+ apt-get install nginx -y
+
  #NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
  #IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
  #cat <<EOF > /var/www/html/index.nginx-debian.html
