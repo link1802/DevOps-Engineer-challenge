1c3732c52bef4a5069dabfda07d13e80d53f84f2
link1802 <link1802@gmail.com>
2022-05-14 00:23:39 -0500

    Update install_nginx.sh

--- Intancia base/install_nginx.sh
+++ Intancia base/install_nginx.sh
@@ -2,10 +2,6 @@
  set -euo pipefail
  export DEBIAN_FRONTEND=noninteractive
  apt-get update -y
- apt-get install ufw -y
- ufw allow 'Nginx HTTP'
- ufw allow 'SSH'
- ufw --force enable
  apt-get install nginx -y
 
  #NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
