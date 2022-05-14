14ccc47959c41934493412bb153f9cdad7bcba3a
link1802 <link1802@gmail.com>
2022-05-14 02:23:45 -0500

    Update install_nginx.sh

--- Intancia base/install_nginx.sh
+++ Intancia base/install_nginx.sh
@@ -5,7 +5,7 @@
  apt-get install nginx -y
  rm /var/www/html/index.nginx-debian.html
  IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
- cat > /var/www/html/index.nginx-debian.html
+ cat > /var/www/html/index.nginx-debian.html 
  cat <<EOF1 > /var/www/html/index.nginx-debian.html
       <html>
       <body>
@@ -15,4 +15,5 @@
       </body>
       </html>
       EOF1
+      ^D
 shutdown now
