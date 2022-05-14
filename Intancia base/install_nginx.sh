1a25a1c59eef6649351092f4bf26d5eaf0080e0c
link1802 <link1802@gmail.com>
2022-05-14 00:00:00 -0500

    script inicial

---
+++ Intancia base/install_nginx.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+ set -euo pipefail
+ export DEBIAN_FRONTEND=noninteractive
+ apt-get update -y
+ apt-get install ufw -y
+ #NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
+ #IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
+ #cat <<EOF > /var/www/html/index.nginx-debian.html
+ #     <pre>
+ #     Name: $NAME
+ #     IP: $IP
+ #     </pre>
+ #     </body>
+ #     </html>
+ #     EOF
--- Intancia base/instancia-base.tf
+++ Intancia base/instancia-base.tf
@@ -19,4 +19,5 @@ resource "google_compute_instance" "default" {
       network_tier = "STANDARD"
     }
   }
+  metadata_startup_script = file("${path.module}/install_nginx.sh")
 }
