c0944d0f6d7036fd81f52da25704a716386ca0b2
link1802 <link1802@gmail.com>
2022-05-14 00:29:13 -0500

    Update install_nginx.sh

--- Intancia base/install_nginx.sh
+++ Intancia base/install_nginx.sh
@@ -4,13 +4,13 @@
  apt-get update -y
  apt-get install nginx -y
 
- #NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
- #IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
- #cat <<EOF > /var/www/html/index.nginx-debian.html
- #     <pre>
- #     Name: $NAME
- #     IP: $IP
- #     </pre>
- #     </body>
- #     </html>
- #     EOF
+ NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
+ IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
+ cat <<EOF1 > /var/www/html/index.nginx-debian.html
+      <pre>
+      Name: $NAME
+      IP: $IP
+      </pre>
+      </body>
+      </html>
+      EOF1
