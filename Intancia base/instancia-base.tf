1a16b3b809f07b7ddf00b0d767268b8d7978a24c
link1802 <link1802@gmail.com>
2022-05-14 15:48:49 -0500

    Update instancia-base.tf

--- Intancia base/instancia-base.tf
+++ Intancia base/instancia-base.tf
@@ -22,14 +22,14 @@ resource "google_compute_instance" "default" {
   metadata_startup_script = file("${path.module}/install_nginx.sh")
 }
 
-resource "time_sleep" "60s" {
+resource "time_sleep" "w60s" {
   depends_on = [google_compute_instance.default]
 
   create_duration = "60s"
 }
 
 resource "google_compute_image" "default" {
-  depends_on = [time_sleep.60s]
+  depends_on = [time_sleep.w60s]
   name = "imagen-base"
   source_disk = google_compute_instance.default.boot_disk[0].source
 }
