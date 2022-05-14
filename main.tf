413e983294747128b8ed44071149e403a46cdd03
link1802 <link1802@gmail.com>
2022-05-13 19:01:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,163 +1,189 @@
-resource "google_compute_autoscaler" "exam" {
-  name   = "autocaler"
-  zone   = "europe-west1-b"
-  target = google_compute_instance_group_manager.mig.id
-
-  autoscaling_policy {
-    max_replicas    = 2
-    min_replicas    = 1
-    cooldown_period = 60
-
-    cpu_utilization {
-      target = 0.4
-    }
-  }
-}
-
-resource "google_compute_address" "static" {
-  name = "ipv4-address"
-  region = "europe-west1"
-  network_tier = "STANDARD"
-}
-################################################################################################
-# VPC network
-resource "google_compute_network" "exam-network" {
-  name                    = "exam-network"
-  provider                = google-beta
-  auto_create_subnetworks = false
-}
-
-# proxy-only subnet
-resource "google_compute_subnetwork" "exam-proxy-subnet" {
-  name          = "exam-proxy-subnet"
-  provider      = google-beta
-  ip_cidr_range = "10.0.0.0/24"
-  region        = "europe-west1"
-  purpose       = "REGIONAL_MANAGED_PROXY"
-  role          = "ACTIVE"
-  network       = google_compute_network.exam-network.id
-}
-
-# backend subnet
-resource "google_compute_subnetwork" "exam-subnet" {
-  name          = "exam-subnet"
-  provider      = google-beta
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "europe-west1"
-  network       = google_compute_network.exam-network.id
-}
-
-# HTTP target proxy
-resource "google_compute_region_target_http_proxy" "default" {
-  name     = "exam-target-http-proxy"
+// Forwarding rule for Internal Load Balancing
+resource "google_compute_forwarding_rule" "default" {
   provider = google-beta
-  region   = "europe-west1"
-  url_map  = google_compute_region_url_map.default.id
-}
+  depends_on = [google_compute_subnetwork.proxy]
+  name   = "website-forwarding-rule"
+  region = "us-central1"
 
-# forwarding rule
-resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
-  name                  = "exam-forwarding-rule"
-  provider              = google-beta
-  region                = "europe-west1"
-  depends_on            = [google_compute_subnetwork.exam-proxy-subnet]
   ip_protocol           = "TCP"
-  load_balancing_scheme = "EXTERNAL_MANAGED"
+  load_balancing_scheme = "INTERNAL_MANAGED"
   port_range            = "80"
   target                = google_compute_region_target_http_proxy.default.id
-  #network               = google_compute_network.exam-network.id
-  #subnetwork            = google_compute_subnetwork.exam-subnet.id
-  network_tier          = "STANDARD"
+  network               = google_compute_network.default.id
+  subnetwork            = google_compute_subnetwork.default.id
+  network_tier          = "PREMIUM"
 }
 
-# URL map
+resource "google_compute_region_target_http_proxy" "default" {
+  provider = google-beta
+
+  region  = "us-central1"
+  name    = "website-proxy"
+  url_map = google_compute_region_url_map.default.id
+}
+
 resource "google_compute_region_url_map" "default" {
-  name            = "exam-regional-url-map"
-  provider        = google-beta
-  region          = "europe-west1"
+  provider = google-beta
+
+  region          = "us-central1"
+  name            = "website-map"
   default_service = google_compute_region_backend_service.default.id
 }
 
-resource "google_compute_region_health_check" "default" {
-  name     = "exam-hc"
+resource "google_compute_region_backend_service" "default" {
   provider = google-beta
-  region   = "europe-west1"
+
+  load_balancing_scheme = "INTERNAL_MANAGED"
+
+  backend {
+    group = google_compute_region_instance_group_manager.rigm.instance_group
+    balancing_mode = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+
+  region      = "us-central1"
+  name        = "website-backend"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_region_health_check.default.id]
+}
+
+data "google_compute_image" "debian_image" {
+  provider = google-beta
+  family   = "debian-9"
+  project  = "debian-cloud"
+}
+
+resource "google_compute_region_instance_group_manager" "rigm" {
+  provider = google-beta
+  region   = "us-central1"
+  name     = "website-rigm"
+  version {
+    instance_template = google_compute_instance_template.instance_template.id
+    name              = "primary"
+  }
+  base_instance_name = "internal-glb"
+  target_size        = 1
+}
+
+resource "google_compute_instance_template" "instance_template" {
+  provider     = google-beta
+  name         = "template-website-backend"
+  machine_type = "e2-medium"
+
+  network_interface {
+    network = google_compute_network.default.id
+    subnetwork = google_compute_subnetwork.default.id
+  }
+
+  disk {
+    source_image = data.google_compute_image.debian_image.self_link
+    auto_delete  = true
+    boot         = true
+  }
+
+  tags = ["allow-ssh", "load-balanced-backend"]
+}
+
+resource "google_compute_region_health_check" "default" {
+  depends_on = [google_compute_firewall.fw4]
+  provider = google-beta
+
+  region = "us-central1"
+  name   = "website-hc"
   http_health_check {
     port_specification = "USE_SERVING_PORT"
   }
 }
 
-# backend service
-resource "google_compute_region_backend_service" "default" {
-  name                  = "exam-backend-subnet"
-  provider              = google-beta
-  region                = "europe-west1"
-  protocol              = "HTTP"
-  load_balancing_scheme = "EXTERNAL_MANAGED"
-  timeout_sec           = 10
-  health_checks         = [google_compute_region_health_check.default.id]
-  backend {
-    group           = google_compute_instance_group_manager.mig.instance_group
-    balancing_mode  = "UTILIZATION"
-    capacity_scaler = 1.0
-  }
-}
-
-# MIG
-resource "google_compute_instance_group_manager" "mig" {
-  name = "group-skydrop"
+resource "google_compute_firewall" "fw1" {
   provider = google-beta
-  zone = "europe-west1-b"
-
-  version {
-    instance_template  = google_compute_instance_template.exam.id
-    name               = "primary"
+  name = "website-fw-1"
+  network = google_compute_network.default.id
+  source_ranges = ["10.1.2.0/24"]
+  allow {
+    protocol = "tcp"
   }
-
-  target_pools       = [google_compute_target_pool.exam.id]
-  base_instance_name = "exam"
-
-  named_port {
-    name = "http"
-    port = 80
+  allow {
+    protocol = "udp"
   }
-
-}
-resource "google_compute_target_pool" "exam" {
-  name = "exam-pool"
-  region = "europe-west1"
-  health_checks = [google_compute_http_health_check.default.name,]
-}
-##############################################################################################
-resource "google_compute_instance_template" "exam" {
-  name           = "instance-template-1"
-  machine_type   = "n2-standard-2"
-  can_ip_forward = true
-
-  network_interface {
-    subnetwork = google_compute_subnetwork.exam-subnet.id
+  allow {
+    protocol = "icmp"
   }
-
-  disk {
-    source_image = data.google_compute_image.debian_11.id
-  }
-
-  service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-  }
-
-metadata_startup_script = file("${path.module}/template/install_nginx.sh")
+  direction = "INGRESS"
 }
 
-resource "google_compute_http_health_check" "default" {
-  name         = "authentication-health-check"
-  request_path = "/health_check"
-
-  timeout_sec        = 1
-  check_interval_sec = 1
+resource "google_compute_firewall" "fw2" {
+  depends_on = [google_compute_firewall.fw1]
+  provider = google-beta
+  name = "website-fw-2"
+  network = google_compute_network.default.id
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  target_tags = ["allow-ssh"]
+  direction = "INGRESS"
 }
 
-data "google_compute_image" "debian_11" {
-  family  = "debian-11"
-  project = "debian-cloud"
+resource "google_compute_firewall" "fw3" {
+  depends_on = [google_compute_firewall.fw2]
+  provider = google-beta
+  name = "website-fw-3"
+  network = google_compute_network.default.id
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+  allow {
+    protocol = "tcp"
+  }
+  target_tags = ["load-balanced-backend"]
+  direction = "INGRESS"
+}
+
+resource "google_compute_firewall" "fw4" {
+  depends_on = [google_compute_firewall.fw3]
+  provider = google-beta
+  name = "website-fw-4"
+  network = google_compute_network.default.id
+  source_ranges = ["10.129.0.0/26"]
+  target_tags = ["load-balanced-backend"]
+  allow {
+    protocol = "tcp"
+    ports = ["80"]
+  }
+  allow {
+    protocol = "tcp"
+    ports = ["443"]
+  }
+  allow {
+    protocol = "tcp"
+    ports = ["8000"]
+  }
+  direction = "INGRESS"
+}
+
+resource "google_compute_network" "default" {
+  provider = google-beta
+  name                    = "website-net"
+  auto_create_subnetworks = false
+  routing_mode = "REGIONAL"
+}
+
+resource "google_compute_subnetwork" "default" {
+  provider = google-beta
+  name          = "website-net-default"
+  ip_cidr_range = "10.1.2.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.default.id
+}
+
+resource "google_compute_subnetwork" "proxy" {
+  provider = google-beta
+  name          = "website-net-proxy"
+  ip_cidr_range = "10.129.0.0/26"
+  region        = "us-central1"
+  network       = google_compute_network.default.id
+  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
+  role          = "ACTIVE"
 }
