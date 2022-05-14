aa61451c597f05bd252e6dc1236d294da837af92
link1802 <link1802@gmail.com>
2022-05-13 22:39:38 -0500

    Update install_nginx.sh

--- template/install_nginx.sh
+++ template/install_nginx.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
-      apt-get update
-      apt-get install -y nginx
-      ufw allow 'Nginx HTTP'
+ set -euo pipefail
+ export DEBIAN_FRONTEND=noninteractive
+ NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
+ IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
+ cat <<EOF > /var/www/html/index.nginx-debian.html
+      <pre>
+      Name: $NAME
+      IP: $IP
+      </pre>
+      </body>
+      </html>
+      EOF
