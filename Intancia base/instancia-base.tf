875ab0cf8868e7729727716451252f86766507b9
link1802 <link1802@gmail.com>
2022-05-13 23:47:02 -0500

    Update instancia-base.tf

--- Intancia base/instancia-base.tf
+++ Intancia base/instancia-base.tf
@@ -11,9 +11,17 @@ resource "google_compute_instance" "default" {
       image = "debian-cloud/debian-11"
     }
   }
-  // Local SSD disk
-  scratch_disk {}
-
+  data "google_compute_image" "debian_image" {
+    provider = google-beta
+    family   = "debian-11"
+    project  = "debian-cloud"
+  }
+  disk {
+    source_image = data.google_compute_image.debian_image.self_link
+    auto_delete  = true
+    boot         = true
+  }
+  
   network_interface {
     network = "default"
 
