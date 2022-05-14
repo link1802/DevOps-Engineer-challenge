cc2d3856a0a34c69a5b7999b887fe8bf79e6d315
link1802 <link1802@gmail.com>
2022-05-14 02:54:57 -0500

    Update instancia-base.tf

--- Intancia base/instancia-base.tf
+++ Intancia base/instancia-base.tf
@@ -21,24 +21,6 @@ resource "google_compute_instance" "default" {
   metadata_startup_script = file("${path.module}/install_nginx.sh")
 }
 
-resource "google_compute_instance" "default2" {
-  name = "instancia-base"
-  machine_type = "e2-micro"
-  desired_status = "TERMINATED"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-  network_interface {
-    network = "default"
-
-    access_config {
-      network_tier = "STANDARD"
-    }
-  }
-}
-
 resource "google_compute_image" "default" {
   name = "imagen-base"
   source_disk = google_compute_instance.default.boot_disk[0].source
