e76f5e411ee9461bb8ce35cdc3ea758b13578939
link1802 <link1802@gmail.com>
2022-05-14 02:26:28 -0500

    Update install_nginx.sh

--- Intancia base/install_nginx.sh
+++ Intancia base/install_nginx.sh
@@ -6,7 +6,7 @@
  rm /var/www/html/index.nginx-debian.html
  IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
  cat > /var/www/html/index.nginx-debian.html 
- cat <<EOF1 > /var/www/html/index.nginx-debian.html
+ cat <<-EOF1 > /var/www/html/index.nginx-debian.html
       <html>
       <body>
       <pre>
@@ -15,5 +15,4 @@
       </body>
       </html>
       EOF1
-      ^D
 shutdown now
