85df01b49ef58a9c8dbab0b9e64170cf74a6ca2b
link1802 <link1802@gmail.com>
2022-05-14 17:18:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -234,7 +234,7 @@ resource "google_compute_subnetwork" "proxy" {
 resource "google_compute_autoscaler" "default" {
   name   = "autoscaler"
   zone   = "us-central1-a"
-  target = google_compute_region_instance_group_manager.rigm.self_link
+  target = google_compute_region_instance_group_manager.rigm.name
 
   autoscaling_policy {
     max_replicas    = 5
