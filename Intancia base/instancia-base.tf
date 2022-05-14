61853e9ce4109b9f36d4a73652457b8b8094718a
link1802 <link1802@gmail.com>
2022-05-14 02:02:14 -0500

    Update instancia-base.tf

--- Intancia base/instancia-base.tf
+++ Intancia base/instancia-base.tf
@@ -23,6 +23,6 @@ resource "google_compute_instance" "default" {
 
 resource "google_compute_image" "default" {
   name = "imagen-base"
-  source_disk = google_compute_instance.default.boot_disk.self_link
+  source_disk = google_compute_instance.default.boot_disk.source
   
 }
