045b8e1e1f9d4809c9070bcff20ca25bd1876681
link1802 <link1802@gmail.com>
2022-05-13 23:44:20 -0500

    Update instancia-base.tf

--- Intancia base/instancia-base.tf
+++ Intancia base/instancia-base.tf
@@ -12,9 +12,7 @@ resource "google_compute_instance" "default" {
     }
   }
   // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
+  scratch_disk {}
 
   network_interface {
     network = "default"
