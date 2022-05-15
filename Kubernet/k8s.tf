46f3244d85a313b3ea1c30e1c8a403597c15852c
link1802 <link1802@gmail.com>
2022-05-15 15:01:40 -0500

    Update k8s.tf

--- Kubernet/k8s.tf
+++ Kubernet/k8s.tf
@@ -1,5 +1,4 @@
 provider "kubernetes" {
-  host                   = "${google_container_cluster.default.endpoint}"
   token                  = "${data.google_client_config.current.access_token}"
   client_certificate     = "${base64decode(google_container_cluster.default.master_auth.0.client_certificate)}"
   client_key             = "${base64decode(google_container_cluster.default.master_auth.0.client_key)}"
