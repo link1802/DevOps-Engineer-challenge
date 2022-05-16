779a3e9394e43e05765426529bd09a445c611f4c
link1802 <link1802@gmail.com>
2022-05-16 07:36:19 -0500

    Update main.tf

--- Kubernet/main.tf
+++ Kubernet/main.tf
@@ -49,13 +49,13 @@ locals {
 #}
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project
-resource "google_project" "k8s-staging" {
+#resource "google_project" "k8s-staging" {
   #name                = local.service_project_name
   #project_id          = local.service_project_id
   #billing_account     = local.billing_account
   #org_id              = local.org_id
   #auto_create_network = false
-}
+#}
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
 resource "google_project_service" "host" {
