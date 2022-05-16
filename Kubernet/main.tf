af74c0abb487f00659d06570899c8debe68c0d59
link1802 <link1802@gmail.com>
2022-05-16 13:02:16 -0500

    v4

--- Kubernet/main.tf
+++ Kubernet/main.tf
@@ -1,60 +1,163 @@
-
-module "gke_auth" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  depends_on   = [module.gke]
-  project_id   = var.project_id
-  location     = module.gke.location
-  cluster_name = module.gke.name
+provider "kubernetes" {
+  version = "~> 1.10.0"
+  host    = google_container_cluster.default.endpoint
+  token   = data.google_client_config.current.access_token
+  client_certificate = base64decode(
+    google_container_cluster.default.master_auth[0].client_certificate,
+  )
+  client_key = base64decode(google_container_cluster.default.master_auth[0].client_key)
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.default.master_auth[0].cluster_ca_certificate,
+  )
 }
-resource "local_file" "kubeconfig" {
 
-  content  = module.gke_auth.kubeconfig_raw
-  filename = "kubeconfig-${var.env_name}"
-}
-module "gcp-network" {
-  source       = "terraform-google-modules/network/google"
-
-  project_id   = var.project_id
-  network_name = "${var.network}-${var.env_name}"
-  subnets = [
-    {
-      subnet_name   = "${var.subnetwork}-${var.env_name}"
-      subnet_ip     = "10.10.0.0/16"
-      subnet_region = var.region
-    },
-  ]
-  secondary_ranges = {
-    "${var.subnetwork}-${var.env_name}" = [
-      {
-        range_name    = var.ip_range_pods_name
-        ip_cidr_range = "10.20.0.0/16"
-      },
-      {
-        range_name    = var.ip_range_services_name
-        ip_cidr_range = "10.30.0.0/16"
-      },
-    ]
+resource "kubernetes_namespace" "staging" {
+  metadata {
+    name = "staging"
   }
 }
 
-module "gke" {
-  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  project_id             = var.project_id
-  name                   = "${var.cluster_name}-${var.env_name}"
-  regional               = true
-  region                 = var.region
-  network                = module.gcp-network.network_name
-  subnetwork             = module.gcp-network.subnets_names[0]
-  ip_range_pods          = var.ip_range_pods_name
-  ip_range_services      = var.ip_range_services_name
-  node_pools = [
-    {
-      name                      = "node-pool"
-      machine_type              = "e2-medium"
-      node_locations            = "europe-west1-b,europe-west1-c,europe-west1-d"
-      min_count                 = 1
-      max_count                 = 2
-      disk_size_gb              = 30
-    },
-  ]
+resource "google_compute_address" "default" {
+  name   = var.network_name
+  region = var.region
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    namespace = kubernetes_namespace.staging.metadata[0].name
+    name      = "nginx"
+  }
+
+  spec {
+    selector = {
+      run = "nginx"
+    }
+
+    session_affinity = "ClientIP"
+
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 80
+    }
+
+    type             = "LoadBalancer"
+    load_balancer_ip = google_compute_address.default.address
+  }
+}
+
+resource "kubernetes_replication_controller" "nginx" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.staging.metadata[0].name
+
+    labels = {
+      run = "nginx"
+    }
+  }
+
+  spec {
+    selector = {
+      run = "nginx"
+    }
+
+    template {
+      container {
+        image = "nginx:latest"
+        name  = "nginx"
+
+        resources {
+          limits {
+            cpu    = "0.5"
+            memory = "512Mi"
+          }
+
+          requests {
+            cpu    = "250m"
+            memory = "50Mi"
+          }
+        }
+      }
+    }
+  }
+}
+
+output "load-balancer-ip" {
+  value = google_compute_address.default.address
+}
+################################################################################
+variable "region" {
+  default = "asia-east1" // Set as per your nearest location or preference 
+}
+
+variable "location" {
+  default = "asia-east1-b"  // Set as per your nearest location or preference 
+}
+
+variable "network_name" {
+  default = "tf-gke-k8s"
+}
+
+provider "google" {
+  region = var.region
+}
+
+resource "google_compute_network" "default" {
+  name                    = var.network_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "default" {
+  name                     = var.network_name
+  ip_cidr_range            = "10.140.0.0/20"  // Change as per your region/zone
+  network                  = google_compute_network.default.self_link
+  region                   = var.region
+  private_ip_google_access = true
+}
+
+data "google_client_config" "current" {
+}
+
+data "google_container_engine_versions" "default" {
+  location = var.location
+}
+
+resource "google_container_cluster" "default" {
+  name               = var.network_name
+  location           = var.location
+  initial_node_count = 3
+  min_master_version = data.google_container_engine_versions.default.latest_master_version
+  network            = google_compute_subnetwork.default.name
+  subnetwork         = google_compute_subnetwork.default.name
+
+  // Use legacy ABAC until these issues are resolved:
+  //   https://github.com/mcuadros/terraform-provider-helm/issues/56
+  //   https://github.com/terraform-providers/terraform-provider-kubernetes/pull/73
+  enable_legacy_abac = true
+// Wait for the GCE LB controller to cleanup the resources.
+  // Wait for the GCE LB controller to cleanup the resources.
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sleep 90"
+  }
+}
+
+output "network" {
+  value = google_compute_subnetwork.default.network
+}
+
+output "subnetwork_name" {
+  value = google_compute_subnetwork.default.name
+}
+
+output "cluster_name" {
+  value = google_container_cluster.default.name
+}
+
+output "cluster_region" {
+  value = var.region
+}
+
+output "cluster_location" {
+  value = google_container_cluster.default.location
 }
