3350ddf4fd48a4067f6973dfe07385f28ea3ef90
link1802 <link1802@gmail.com>
2022-05-16 11:05:28 -0500

    Update main.tf

--- Kubernet/main.tf
+++ Kubernet/main.tf
@@ -1,256 +1,61 @@
-##################################################################################################
-# Google Cloud Platform Provider
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs
 provider "google" {
-  region = "us-west2"
+  version = "~> 3.42.0"
 }
-
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/integer
-resource "random_integer" "int" {
-  min = 100
-  max = 1000000
+module "gke_auth" {
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  depends_on   = [module.gke]
+  project_id   = var.project_id
+  location     = module.gke.location
+  cluster_name = module.gke.name
 }
-
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 3.66"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1"
-    }
-  }
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig-${var.env_name}"
 }
-################################################################################################
-locals {
-  region               = "us-west2"
-  #org_id               = "skydropx-devops-challenge"
-  #billing_account      = "6753-6079-2119-1468"
-  #host_project_name    = "host-staging"
-  #service_project_name = "k8s-staging"
-  #host_project_id      = "${local.host_project_name}-${random_integer.int.result}"
-  #service_project_id   = "${local.service_project_name}-${random_integer.int.result}"
-  projects_api         = "container.googleapis.com"
-  secondary_ip_ranges = {
-    "pod-ip-range"      = "10.0.0.0/14",
-    "services-ip-range" = "10.4.0.0/19"
-  }
-}
-################################################################################################
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project
-#resource "google_project" "host-staging" {
-  #name                = local.host_project_name
-  #project_id          = local.host_project_id
-  #billing_account     = local.billing_account
-  #org_id              = local.org_id
-  #auto_create_network = false
-#}
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project
-#resource "google_project" "k8s-staging" {
-  #name                = local.service_project_name
-  #project_id          = local.service_project_id
-  #billing_account     = local.billing_account
-  #org_id              = local.org_id
-  #auto_create_network = false
-#}
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
-#resource "google_project_service" "host" {
-  #project = google_project.host-staging.number
-  #service = local.projects_api
-#}
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service
-#resource "google_project_service" "service" {
-  #project = google_project.k8s-staging.number
-  #service = local.projects_api
-#}
-######################################################################################################
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
-resource "google_compute_network" "main" {
-  name                    = "main"
-  #project                 = google_compute_shared_vpc_host_project.host.project
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
-  mtu                     = 1500
-}
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
-resource "google_compute_subnetwork" "private" {
-  name                     = "private"
-  #project                  = google_compute_shared_vpc_host_project.host.project
-  ip_cidr_range            = "10.5.0.0/20"
-  region                   = local.region
-  network                  = google_compute_network.main.self_link
-  private_ip_google_access = true
-
-  dynamic "secondary_ip_range" {
-    for_each = local.secondary_ip_ranges
-
-    content {
-      range_name    = secondary_ip_range.key
-      ip_cidr_range = secondary_ip_range.value
-    }
-  }
-}
-######################################################################################################
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router
-resource "google_compute_router" "router" {
-  name    = "router"
-  region  = local.region
-  #project = local.host_project_id
-  network = google_compute_network.main.self_link
-}
-#########################################################################################################
-resource "google_compute_router_nat" "mist_nat" {
-  name                               = "nat"
-  #project                            = local.host_project_id
-  router                             = google_compute_router.router.name
-  region                             = local.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-
-  depends_on = [google_compute_subnetwork.private]
-}
-###############################################################################################
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_shared_vpc_host_project
-#resource "google_compute_shared_vpc_host_project" "host" {
-#  project = google_project.host-staging.number
-#}
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_shared_vpc_service_project
-#resource "google_compute_shared_vpc_service_project" "service" {
-#  host_project    = local.host_project_id
-#  service_project = local.service_project_id
-#
-#  depends_on = [google_compute_shared_vpc_host_project.host]
-#}
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork_iam
-#resource "google_compute_subnetwork_iam_binding" "binding" {
-  #project    = google_compute_shared_vpc_host_project.host.project
-  #region     = google_compute_subnetwork.private.region
-  #subnetwork = google_compute_subnetwork.private.name
-
-  #role = "roles/compute.networkUser"
-  #members = [
-  #  "serviceAccount:${google_service_account.k8s-staging.email}",
-  #  "serviceAccount:${google_project.k8s-staging.number}@cloudservices.gserviceaccount.com",
-  #  "serviceAccount:service-${google_project.k8s-staging.number}@container-engine-robot.iam.gserviceaccount.com"
-  #]
-#}
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
-#resource "google_project_iam_binding" "container-engine" {
-  #project = google_compute_shared_vpc_host_project.host.project
-  #role    = "roles/container.hostServiceAgentUser"
-
-  #members = [
-  #  "serviceAccount:service-${google_project.k8s-staging.number}@container-engine-robot.iam.gserviceaccount.com",
-  #]
-  #depends_on = [google_project_service.service]
-#}
-#########################################################################################################
-#resource "google_service_account" "k8s-staging" {
-#  project    = local.service_project_id
-#  account_id = "k8s-staging"
-
-#  depends_on = [google_project.k8s-staging]
-#}
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
-resource "google_container_cluster" "gke" {
-  name     = "gke"
-  location = local.region
-#  project  = local.service_project_id
-
-  networking_mode = "VPC_NATIVE"
-  network         = google_compute_network.main.self_link
-  subnetwork      = google_compute_subnetwork.private.self_link
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  release_channel {
-    channel = "REGULAR"
-  }
-
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "pod-ip-range"
-    services_secondary_range_name = "services-ip-range"
-  }
-
-  network_policy {
-    provider = "PROVIDER_UNSPECIFIED"
-    enabled  = true
-  }
-
-  private_cluster_config {
-    enable_private_endpoint = false
-    enable_private_nodes    = true
-    master_ipv4_cidr_block  = "172.16.0.0/28"
-  }
-
-  workload_identity_config {
-    identity_namespace = "skydropx-devops-challenge.svc.id.goog"
-  }
-
-}
-
-resource "google_container_node_pool" "general" {
-  name       = "general"
-  location   = local.region
-  cluster    = google_container_cluster.gke.name
-#  project    = local.service_project_id
-  node_count = 1
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    labels = {
-      role = "general"
-    }
-    machine_type = "e2-medium"
-
-#    service_account = google_service_account.k8s-staging.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
+module "gcp-network" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 2.5"
+  project_id   = var.project_id
+  network_name = "${var.network}-${var.env_name}"
+  subnets = [
+    {
+      subnet_name   = "${var.subnetwork}-${var.env_name}"
+      subnet_ip     = "10.10.0.0/16"
+      subnet_region = var.region
+    },
+  ]
+  secondary_ranges = {
+    "${var.subnetwork}-${var.env_name}" = [
+      {
+        range_name    = var.ip_range_pods_name
+        ip_cidr_range = "10.20.0.0/16"
+      },
+      {
+        range_name    = var.ip_range_services_name
+        ip_cidr_range = "10.30.0.0/16"
+      },
     ]
   }
 }
-###########################################################################################
-resource "google_compute_firewall" "lb" {
-  name        = "k8s-fw-abdca8a7bd83f4a84a8fb7a869242967"
-  network     = google_compute_network.main.name
-#  project     = local.host_project_id
-  description = "{\"kubernetes.io/service-name\":\"default/nginx\", \"kubernetes.io/service-ip\":\"35.235.121.183\"}"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["gke-gke-08c5d5fb-node"]
-}
-
-resource "google_compute_firewall" "health" {
-  name        = "k8s-9cf34e5201ee63aa-node-http-hc"
-  network     = google_compute_network.main.name
-#  project     = local.host_project_id
-  description = "{\"kubernetes.io/cluster-id\":\"9cf34e5201ee63aa\"}"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["10256"]
-  }
-
-  source_ranges = ["130.211.0.0/22", "209.85.152.0/22", "209.85.204.0/22", "35.191.0.0/16"]
-  target_tags   = ["gke-gke-08c5d5fb-node"]
+module "gke" {
+  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  project_id             = var.project_id
+  name                   = "${var.cluster_name}-${var.env_name}"
+  regional               = true
+  region                 = var.region
+  network                = module.gcp-network.network_name
+  subnetwork             = module.gcp-network.subnets_names[0]
+  ip_range_pods          = var.ip_range_pods_name
+  ip_range_services      = var.ip_range_services_name
+  node_pools = [
+    {
+      name                      = "node-pool"
+      machine_type              = "e2-medium"
+      node_locations            = "europe-west1-b,europe-west1-c,europe-west1-d"
+      min_count                 = 1
+      max_count                 = 2
+      disk_size_gb              = 30
+    },
+  ]
 }
