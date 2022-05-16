8e1ed7dadb8f447209e150e04c958f49d7350fdb
link1802 <link1802@gmail.com>
2022-05-16 13:03:22 -0500

    Update main.tf

--- Kubernet/main.tf
+++ Kubernet/main.tf
@@ -1,5 +1,4 @@
 provider "kubernetes" {
-  version = "~> 1.10.0"
   host    = google_container_cluster.default.endpoint
   token   = data.google_client_config.current.access_token
   client_certificate = base64decode(
