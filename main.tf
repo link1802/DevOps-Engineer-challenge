553fcbdaeeb2b13ff6d1a29128d081fcc78c4ac4
link1802 <link1802@gmail.com>
2022-05-17 15:00:16 -0500

    update

--- main.tf
+++ main.tf
@@ -1,42 +1,46 @@
+
+//creation of VM before create a template
 resource "google_compute_instance" "default" {
-  name         = "instancia-base"
+  name         = var.vm_base_name
   machine_type = var.machine_type
   allow_stopping_for_update = true
   tags = ["allow-ssh", "load-balanced-backend", "http-server", "https-server"]
   
-  
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.vm_base_os
     }
   }
-  
+  //default interface for updates
   network_interface {
     network = "default"
-
     access_config {
       network_tier = "STANDARD"
     }
   }
+  //script to install nginx on the VM
   metadata_startup_script = file("${path.module}/scripts/install_nginx.sh")
 }
+//////////////////////////////////////////////////////////////////////////////////
 
+//delay for the shutdown of instance
 resource "time_sleep" "w60s" {
   depends_on = [google_compute_instance.default]
   create_duration = "60s"
 }
 
+//create the template on base a VM shutdown
 resource "google_compute_image" "default" {
   depends_on = [time_sleep.w60s]
-  name = "imagen-base"
+  name = var.vm_image
   source_disk = google_compute_instance.default.boot_disk[0].source
 }
-///////////////////////////////////////////////////////////////////////////////////////
+
+///////////rules of firewall and networking//////////////
 // Forwarding rule for Regional External Load Balancing
 resource "google_compute_forwarding_rule" "default" {
   depends_on = [google_compute_subnetwork.proxy]
   name   = "website-forwarding-rule"
-
   ip_protocol           = "TCP"
   load_balancing_scheme = "EXTERNAL_MANAGED"
   port_range            = "80"
@@ -57,74 +61,44 @@ resource "google_compute_region_url_map" "default" {
 }
 
 resource "google_compute_region_backend_service" "default" {
-
   load_balancing_scheme = "EXTERNAL_MANAGED"
-
   backend {
     group = google_compute_instance_group_manager.rigm.instance_group
     balancing_mode = "UTILIZATION"
     capacity_scaler = 1.0
   }
-
   name        = "website-backend"
   protocol    = "HTTP"
   timeout_sec = 10
   health_checks = [google_compute_region_health_check.default.id]
 }
 
-
-
-resource "google_compute_instance_group_manager" "rigm" {
-
-  name     = "website-rigm"
-  version {
-    instance_template = google_compute_instance_template.instance_template.id
-    name              = "primary"
-  }
-  named_port {
-    name = "http"
-    port = 80
-  }
-  base_instance_name = "internal-glb"
-  target_size        = 2
+resource "google_compute_network" "default" {
+  name                    = "website-net"
+  auto_create_subnetworks = false
+  routing_mode = "REGIONAL"
 }
 
-resource "google_compute_instance_template" "instance_template" {
-  name         = "template-website-backend"
-  machine_type = "e2-micro"
-
-  network_interface {
-    network = google_compute_network.default.id
-    subnetwork = google_compute_subnetwork.default.id
-  }
-
-  disk {
-    source_image = google_compute_image.default.self_link
-    auto_delete  = true
-    boot         = true
-  }
-  metadata_startup_script = file("${path.module}/scripts/config_ip_resp.sh")
-
-  tags = ["allow-ssh", "load-balanced-backend", "http-server", "https-server"]
+resource "google_compute_subnetwork" "default" {
+  name          = "website-net-default"
+  ip_cidr_range = var.ip_range_lan
+  region        = var.proyect_region
+  network       = google_compute_network.default.id
 }
 
-resource "google_compute_region_health_check" "default" {
-  depends_on = [google_compute_firewall.fw4]
-  name   = "website-hc"
-  tcp_health_check {
-    port = "80"
-  }
+resource "google_compute_subnetwork" "proxy" {
+  name          = "website-net-proxy"
+  ip_cidr_range = var.ip_range_proxy
+  #region        = var.proyect_region
+  network       = google_compute_network.default.id
+  purpose       = "REGIONAL_MANAGED_PROXY"
+  role          = "ACTIVE"
 }
 
-resource "google_compute_address" "default" {
-  name = "website-ip-1"
-  network_tier = "STANDARD"
-}
-
-resource "google_compute_firewall" "fw1" {
+resource "google_compute_firewall" "rule1" {
   name = "website-fw-1"
   network = google_compute_network.default.id
-  source_ranges = ["10.1.2.0/24"]
+  source_ranges = [var.ip_range_lan]
   allow {
     protocol = "tcp"
   }
@@ -137,8 +111,8 @@ resource "google_compute_firewall" "fw1" {
   direction = "INGRESS"
 }
 
-resource "google_compute_firewall" "fw2" {
-  depends_on = [google_compute_firewall.fw1]
+resource "google_compute_firewall" "rule2" {
+  depends_on = [google_compute_firewall.rule1]
   name = "website-fw-2"
   network = google_compute_network.default.id
   source_ranges = ["0.0.0.0/0"]
@@ -150,23 +124,23 @@ resource "google_compute_firewall" "fw2" {
   direction = "INGRESS"
 }
 
-resource "google_compute_firewall" "fw3" {
-  depends_on = [google_compute_firewall.fw2]
-  name = "website-fw-3"
-  network = google_compute_network.default.id
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
-  allow {
-    protocol = "tcp"
-  }
-  target_tags = ["load-balanced-backend"]
-  direction = "INGRESS"
-}
+#resource "google_compute_firewall" "fw3" {
+#  depends_on = [google_compute_firewall.rule2]
+#  name = "website-fw-3"
+#  network = google_compute_network.default.id
+#  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+#  allow {
+#    protocol = "tcp"
+#  }
+#  target_tags = ["load-balanced-backend"]
+#  direction = "INGRESS"
+#}
 
-resource "google_compute_firewall" "fw4" {
-  depends_on = [google_compute_firewall.fw3]
+resource "google_compute_firewall" "rule3" {
+  depends_on = [google_compute_firewall.rule2]
   name = "website-fw-4"
   network = google_compute_network.default.id
-  source_ranges = ["10.129.0.0/26"]
+  source_ranges = [var.ip_range_proxy]
   target_tags = ["load-balanced-backend"]
   allow {
     protocol = "tcp"
@@ -183,31 +157,58 @@ resource "google_compute_firewall" "fw4" {
   direction = "INGRESS"
 }
 
-resource "google_compute_network" "default" {
-  name                    = "website-net"
-  auto_create_subnetworks = false
-  routing_mode = "REGIONAL"
+////Creation of instance template////////////////////////////////////
+resource "google_compute_instance_group_manager" "rigm" {
+  name     = "website-rigm"
+  version {
+    instance_template = google_compute_instance_template.default.id
+    name              = "primary"
+  }
+  named_port {
+    name = "http"
+    port = 80
+  }
+  base_instance_name = "internal-glb"
+  target_size        = var.instance_group_size
 }
 
-resource "google_compute_subnetwork" "default" {
-  name          = "website-net-default"
-  ip_cidr_range = "10.1.2.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.default.id
+resource "google_compute_instance_template" "default" {
+  name         = "template-website-backend"
+  machine_type = var.machine_type
+
+  network_interface {
+    network = google_compute_network.default.id
+    subnetwork = google_compute_subnetwork.default.id
+  }
+
+  disk {
+    source_image = google_compute_image.default.self_link
+    auto_delete  = true
+    boot         = true
+  }
+  //script to add the ip to nginx
+  metadata_startup_script = file("${path.module}/scripts/config_ip_resp.sh")
+
+  tags = ["allow-ssh", "load-balanced-backend", "http-server", "https-server"]
 }
 
-resource "google_compute_subnetwork" "proxy" {
-  name          = "website-net-proxy"
-  ip_cidr_range = "10.129.0.0/26"
-  #region        = "us-central1"
-  network       = google_compute_network.default.id
-  purpose       = "REGIONAL_MANAGED_PROXY"
-  role          = "ACTIVE"
+resource "google_compute_region_health_check" "default" {
+  depends_on = [google_compute_firewall.rule3]
+  name   = "website-hc"
+  tcp_health_check {
+    port = "80"
+  }
 }
+
+#resource "google_compute_address" "default" {
+#  name = "website-ip-1"
+#  network_tier = "STANDARD"
+#}
+
+
 
 resource "google_compute_autoscaler" "default" {
   name   = "autoscaler"
-  #zone   = "us-central1-a"
   target = google_compute_instance_group_manager.rigm.id
   autoscaling_policy {
     max_replicas    = 2
--- variables.tf
+++ variables.tf
@@ -9,9 +9,47 @@ provider "google" {
   region      = "us-central1"
   zone        = "us-central1-a"
 }
-
+/////////////////////////////////////////////////////
+variable "proyect_region" {
+  description = "the regon of proyect"
+  type        = string
+  default     = "us-central1"
+}
+///////////////variables of VM///////////////////////
 variable "machine_type" {
   description = "the type of machine on GCP"
   type        = string
   default     = "e2-micro"
+}
+variable "vm_base_name" {
+  description = "name of vm"
+  type        = string
+  default     = "vm_base"
+}
+variable "vm_base_os" {
+  description = "the os of the vm"
+  type        = string
+  default     = "debian-cloud/debian-11"
+}
+variable "vm_image" {
+  description = "the image of the VM"
+  type        = string
+  default     = "debian-cloud/debian-11"
+}
+///////////variables of Networking/////////////////////
+variable "ip_range_lan" {
+  description = "range of lan"
+  type        = string
+  default     = "10.1.2.0/24"
+}
+variable "ip_range_proxy" {
+  description = "range of proxy"
+  type        = string
+  default     = "10.129.0.0/26"
+}
+/////////////////////////////////////////////////////
+variable "instance_group_size" {
+  description = "size of instance group"
+  type        = number
+  default     = 2
 }
