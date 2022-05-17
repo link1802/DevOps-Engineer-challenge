c012a99f994785a8dd86ba806335a5306e1a454f
link1802 <link1802@gmail.com>
2022-05-17 14:06:29 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,9 @@ provider "google" {
   region      = "us-central1"
   zone        = "us-central1-a"
 }
+
+variable "machine_type" {
+  description = "the type of machine on GCP"
+  type        = string
+  default     = "e2-micro"
+}
