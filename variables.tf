6aaa84f4f5c3909bc5b4715f703d1db8985ecee2
link1802 <link1802@gmail.com>
2022-05-17 20:02:30 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ provider "google" {
 variable "proyect_id" {
   description = "the name of proyect"
   type        = string
-  default     = "devops-engineer-challenge-10"
+  default     = "devops-engineer-challenge-11"
 }
 variable "proyect_region" {
   description = "the region of proyect"
