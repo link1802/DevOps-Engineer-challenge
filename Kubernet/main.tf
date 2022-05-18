1bdbbb607df847d54390add54d6b2c75c5b0d24a
link1802 <link1802@gmail.com>
2022-05-18 00:19:48 -0500

    Update main.tf

--- Kubernet/main.tf
+++ Kubernet/main.tf
@@ -196,9 +196,6 @@ resource "google_container_cluster" "default" {
 // Wait for the GCE LB controller to cleanup the resources.
   // Wait for the GCE LB controller to cleanup the resources.
   provisioner "local-exec" {
-    command = "cat > /usr/share/nginx/html/index.html; cat ´ejemplo´> /usr/share/nginx/html/index.html"
-  }
-  provisioner "local-exec" {
     when    = destroy
     command = "sleep 90"
   }
